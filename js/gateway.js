a7807ffbdf45da43554bd096f56f19ad7ccb21b8
TechNikh <info@technikh.com>
2019-02-09 07:48:55 -0500

    initial working commit

---
+++ js/gateway.js
@@ -0,0 +1,116 @@
+// Global variables
+var $socket = null;
+var $remotePeer = null;
+$serverUrl = 'https://video-pc-app.herokuapp.com';
+
+// Handle the commands from the extension
+function actionHandler(data) {
+    console.log(data)
+    switch (data.payload.cmd) {
+        // Update list of tabs 
+        case "tabsListUpdate":
+            updateTabs(data.payload.params.tabsList);
+            break;
+        // Change current tab
+        case "currentTabUpdate":
+            setCurrentTabID(data.payload.params.id);
+            break;
+        // Audible change
+        case "audibleUpdate":
+            setAudibleChanged(data.payload.params.id, data.payload.params.audible);
+            break;
+
+        default:
+            break;
+    }
+}
+
+// Send a command to the socket
+function sendCommand(payload) {
+    $socket.emit("send", { id: $remotePeer, payload: payload });
+}
+
+// Connect the extension to the socket server
+function connect(peerID) {
+    // Set remote peer id
+    $remotePeer = peerID;
+
+    // Open socket
+    $socket = io($serverUrl);
+
+    // Trigger when the connection was made
+    $socket.on('connect', function() {
+        console.log("connected");
+        $socket.emit('join', { id : peerID });
+    });
+
+    // Trigger when another user joined
+    $socket.on('join', (data) => {
+        console.log("successfully connected", data)
+        connectionStarted()
+    });
+
+    // Trigger when a command was received
+    $socket.on('receive', (data) => {
+        // Handle the command
+        actionHandler(data);
+    });
+
+    // Trigger when the app disconnected
+    $socket.on('user-disconnected', function() {
+        // Handle the command
+        connectionEnded();
+    });
+}
+
+// Trigger when connection started
+function connectionStarted() {
+    var connectSection = document.getElementById("connectSection");
+    var panelSection = document.getElementById("panelSection");
+
+    // Send the first tab update
+    sendCommand({ cmd: "tabsListUpdate" });
+
+    // Update UI
+    connectSection.style.display = "none";
+    panelSection.style.display = "block";
+}
+
+// Trigger when connection ended
+function connectionEnded() {
+    var connectSection = document.getElementById("connectSection");
+    var panelSection = document.getElementById("panelSection");
+    var openedTabsList = document.getElementById("openedTabsList");
+
+    // Update UI
+    connectSection.style.display = "block";
+    panelSection.style.display = "none";
+    openedTabsList.innerHTML = "";
+}
+
+// Disconnect from server
+function disconnect() {
+    $socket.disconnect();
+    connectionEnded();
+}
+
+// Close a tab
+function closeTab(id) {
+    sendCommand({ cmd: "closeTab", params: { id: id } });
+}
+
+// Change a tab
+function changeTab(id) {
+    sendCommand({ cmd: "changeTab", params: { id: id } });
+}
+
+// Play a video
+function playTab(id) {
+    sendCommand({ cmd: "playTab", params: { id: id } });
+}
+
+// Open a new tab
+function newTab(url) {
+    sendCommand({ cmd: "newTab", params: { url : url } });
+}
+
---
+++ js/remote.js
@@ -0,0 +1,169 @@
+// Global variables
+$currentTabID = -1;
+$tabsList = [];
+
+// Update the tabs 
+function updateTabs(list) {
+    var container = document.getElementById("openedTabsList");
+    var html = "";
+    $tabsList = [];
+
+    if (list.length > 0) {
+        // If the list of tabs contains at least 1 tab, display them
+        for (var item in list) {
+            var element = list[item];
+            // Display each tab 
+            html += `<div class="input-group mb-3">
+                        <input type="text" class="form-control focusTabButton" data-tabID="` + element.id + `" readonly value="` + element.title + `">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary closeTabButton" type="button" data-tabID="` + element.id + `">X</button>
+                        </div>
+                    </div>`;
+
+            // If the tab is active change the UI accordingly
+            if (element.active){
+                $currentTabID = element.id;
+                
+                // Change Play button text
+                document.getElementById("playButton").innerHTML = element != null && element.audible ? "Pause Video" : "Play Video";
+                // Enable/Disable play button             
+                document.getElementById("playButton").disabled = element == null ? true : false;
+                // Show/hide error message
+                document.getElementById("activeTab").innerHTML = element == null ? "You must select a video tab first" : "<b>" + element.title + "</b>";  
+            }
+
+            // Add the tabs to the local list
+            $tabsList.push(element);
+        }
+    } else {
+        // Display a default message when there is no video tab
+        html += `<div>You have no video tabs</div>`;
+    }
+
+    // Apply the changes
+    container.innerHTML = html;
+
+    Array.from(container.getElementsByClassName("closeTabButton")).forEach(element => {
+        // Add event listeners for "close" buttons
+        element.addEventListener("click", function(params) {
+            var tabID = this.getAttribute("data-tabID");
+            
+            // Send command to close the tab
+            closeTab(tabID);
+        });
+    });
+
+    Array.from(container.getElementsByClassName("focusTabButton")).forEach(element => {
+        // Add event listeners for "focus" buttons
+        element.addEventListener("click", function(params) {
+            var tabID = this.getAttribute("data-tabID");
+            
+            // Send command to change the tab
+            changeTab(tabID);
+        });
+    })
+}
+
+// Change the UI when the tab has changed
+function setCurrentTabID(id) {
+    $currentTabID = id;
+    var tab = getTabById(id);
+
+    // Change Play button text
+    document.getElementById("playButton").innerHTML = tab != null && tab.audible ? "Pause Video" : "Play Video"; 
+    // Enable/Disable play button  
+    document.getElementById("playButton").disabled = tab == null ? true : false;
+    // Show/hide error message
+    document.getElementById("activeTab").innerHTML = tab == null ? "You must select a video tab first" : "<b>" + tab.title + "</b>";  
+}
+
+// Change the UI when the audible has changed (sound)
+function setAudibleChanged(id, audible) {
+    var tab = getTabById(id);
+
+    // Change Play button text
+    document.getElementById("playButton").innerHTML = tab != null && audible ? "Pause Video" : "Play Video";
+}
+
+// Open and process QR Code image
+function openQRCamera(node, elementID) {
+    var reader = new FileReader();
+
+    reader.onload = function() {
+        node.value = "";
+
+        qrcode.callback = function(res) {
+            if(res instanceof Error) {
+                // Error - QR code couldn't be found
+                alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
+            } else {
+                // QR code was found
+                elementID.value = res;
+            }
+        };
+
+        qrcode.decode(reader.result);
+    };
+
+    reader.readAsDataURL(node.files[0]);
+}
+
+// Get tab by id, from local tab list
+function getTabById(id) {
+    var tab = null;
+
+    for (const key in $tabsList) {
+        const element = $tabsList[key];
+        if (element.id == id) {
+            tab = element;
+            break;
+        }
+    }
+
+    return tab;
+}
+
+// Add event listener to "Play" button
+document.getElementById("playButton").addEventListener("click", function(params) {
+    // FixMe - adding an attribute on playButton which will show the status
+    document.getElementById("playButton").innerHTML = document.getElementById("playButton").innerHTML == "Play Video"? "Pause Video" : "Play Video";
+    playTab($currentTabID);    
+});
+
+// Add event listener to "New tab" button
+document.getElementById("newTabButton").addEventListener("click", function(params) {
+    // Math the domain
+    var urlInput = document.getElementById("urlInput");
+    var matches = urlInput.value.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    var domain = matches && matches[1];
+
+    // Check if domain is valid
+    if (domain == "www.youtube.com" || domain == "youtu.be" || domain == "www.youtu.be") {
+        newTab(urlInput.value);
+    } else {
+        alert("Your video URL appears to be invalid");
+    }
+});
+
+// Add event listener for "scan" button
+document.getElementById("scanButton").addEventListener("change", function() {
+    openQRCamera(this, document.getElementById("connectInput"));
+});
+
+// Add event listener for "connect" button
+document.getElementById("connectButton").addEventListener("click", function() {
+    var code = document.getElementById("connectInput").value;
+
+    if (code.length < 10) {
+        alert("Your code is too short");
+    } else {
+        // If code is valid send connect command
+        connect(code); 
+    }  
+});
+
+// Add event listener for "disconnect" button
+document.getElementById("disconnectButton").addEventListener("click", function() {
+    // Disconnect the user
+    disconnect();
+});
